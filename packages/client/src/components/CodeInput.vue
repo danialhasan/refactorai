2443236f82f5a7671979eb50e31355d6927f5335
Danial Hasan <danialhasan14@gmail.com>
2023-03-06 20:20:08 -0500

    add tailwindcss

--- packages/client/src/components/CodeInput.vue
+++ packages/client/src/components/CodeInput.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h1 class="">CODE INPUT</h1>
+        <h1 class="text-blue-500">CODE INPUT</h1>
+        <h2 class="text-xl">this is some more text</h2>
     </div>
 </template>
 <script></script>
