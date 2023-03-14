045c290eb150018d85a039421cfbae080aa884c5
Danial Hasan <danialhasan14@gmail.com>
2023-03-14 18:38:47 -0400

    prepare codebase for pinnia

--- packages/extension/src/extension.ts
+++ packages/extension/src/extension.ts
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as path from "path";
-import { TextEncoder } from "util";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
